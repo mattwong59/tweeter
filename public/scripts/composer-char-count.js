989e3f51a2932b6d270b4ee7213d79f5e40d33f0
mattwong59 <mattwong59@gmail.com>
2018-03-27 20:24:44 +0000

    Added red counter when count < 0

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -1,9 +1,13 @@
 $ (document).ready(function() {
  $('textarea').on('keyup', function () {
     var count = this.textLength;
-    console.log('count:', count);
-    var charCounter = $(this).parent().find(".counter")
+    var charCounter = $(this).parent().find(".counter");
     charCounter.text(140 - count);
 
- })
+    if(count > 140) {
+      $(".counter").css("color", "tomato")
+    } else {
+      $(".counter").css("color", "black")
+    }
+ });
 });
