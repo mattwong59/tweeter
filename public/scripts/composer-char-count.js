cd58b548cdfb22e1edb06f141130df3aad6e6a39
mattwong59 <mattwong59@gmail.com>
2018-03-27 21:00:11 +0000

    Added section for all tweets in index.html

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -1,6 +1,6 @@
 $ (document).ready(function() {
- $('textarea').on('keyup', function () {
-    var count = this.textLength;
+ $('textarea').on('input', function () {
+    var count = $(this).val().length;
     var charCounter = $(this).parent().find(".counter");
     charCounter.text(140 - count);
 
