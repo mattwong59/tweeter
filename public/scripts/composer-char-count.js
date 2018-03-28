287e423e5a96c741a9b5a3162109fb506d805663
mattwong59 <mattwong59@gmail.com>
2018-03-28 00:23:02 +0000

    Added styled tweet

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -1,8 +1,8 @@
-$ (document).ready(function() {
- $('textarea').on('input', function () {
-    var count = $(this).val().length;
-    var charCounter = $(this).parent().find(".counter");
-    charCounter.text(140 - count);
+
+$(function() {
+ $('textarea').on('input', function (event) {
+    var count = $(event.target).val().length;
+    var charCounter = $(event.target).parent().find(".counter").text(140 - count);
 
     if(count > 140) {
       $(".counter").css("color", "tomato")
