05b55da6bbfd69a5fda3274e2f5f5f94c67dafcf
mattwong59 <mattwong59@gmail.com>
2018-03-28 04:43:10 +0000

    Added jquery template of html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,29 +20,59 @@ const tweetData = {
 }
 
 $(function() {
-  function createTweetElement(tweet) {
-    let $tweet = $('<article>').addClass('tweet');
-    let $header = $('<header>');
-    let $username = $('<h2>').text(tweet.user.name);
-    let $handle = $('<h4>').text(tweet.handle)
-    let $avatar = $('<img>')
-    let $content = $('<p>').text(tweet.content.text);
-    let $footer = $('<footer>');
-    let $date = $('<h5>').text(tweet.created_at);
 
-    $('header').appendTo('tweet');
-    $('username').appendTo('header');
-    $('handle').appendTo('header');
-    $('avatar').appendTo('header')
-    $('content').appendTo('tweet');
-    $('footer').appendTo('tweet');
-    $('date').appendTo('footer');
+  var $tweet = createTweetElement(tweetData);
+
+  function createTweetElement(tweet) {
+
+    let username = tweet.user.name;
+    let handle = tweet.handle;
+    let avatar = tweet.user.avatars.small;
+    let content = tweet.content.text;
+    let day = new Date(tweet.created_at);
+    let today = new Date();
+    let oneDay = 24*60*60*1000;
+    let days = Math.round(Math.abs((day-today)/oneDay)) + ' days ago.'
+
+    let $tweet = $('<article>').addClass('tweet');
+
+    let $header = $('<header>');
+    $tweet.append($header);
+
+    let $username = $('<h2>');
+    $username.text(username);
+    $header.append($username);
+
+    let $handle = $('<h4>');
+    $handle.text(handle);
+    $header.append($handle);
+
+    let $avatar = $('<img>').addClass('avatar');
+    $avatar.attr('src', avatar);
+    $header.append($avatar);
+
+    let $content = $('<p>')
+    $tweet.append($content);
+
+    let $footer = $('<footer>');
+    $tweet.append($footer);
+
+    let $days = $('<p>').addClass("Days");
+    $days.text(days);
+    $footer.append($days);
+
+
+    let $date = $('<h5>')
+    let $iconHeart = $('<i>').addClass('icon fas fa-heart');
+    let $iconRetweet = $('<i>').addClass('icon fas fa-retweet');
+    let $iconFlagg = $('<i>').addClass('icon fas fa-flag');
+
 
     return $tweet;
     //console.log($tweet);
 
   }
-var $tweet = createTweetElement(tweetData);
+
 $('#tweets-container').append($tweet);
 
 // Test / driver code (temporary)
