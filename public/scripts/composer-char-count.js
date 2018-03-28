61a6de47aff138d303a30040ced3d9325782332f
mattwong59 <mattwong59@gmail.com>
2018-03-28 19:12:44 +0000

    Cleaned up code to method chain

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -1,8 +1,7 @@
 $(function() {
  $('textarea').on('input', function (event) {
     var count = $(event.target).val().length;
-    var charCounter = $(event.target).parent().find(".counter")
-    charCounter.text(140 - count);
+    var charCounter = $(event.target).parent().find(".counter").text(140 - count);
 
     if(count > 140) {
       $(".counter").css("color", "tomato")
